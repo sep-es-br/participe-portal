55cd9c6024c74c0a1407133592387c725e400c6f
Vagner Dargan Cordeiro <41954965+vdcordeiro@users.noreply.github.com>
2020-07-05 17:15:03 -0300

    Sorting filters options

--- src/app/pages/proposals/proposals.component.ts
+++ src/app/pages/proposals/proposals.component.ts
@@ -102,6 +102,18 @@ export class ProposalsComponent implements OnInit {
     if(success){
       this.regionName = data.regionName;
       this.itemName = data.itemName;
+      
+      data.items.sort( function( a, b ) {
+                        if ( a.name < b.name ){
+                          return -1;
+                        }
+                        if ( a.name > b.name ){
+                          return 1;
+                        }
+                        return 0;
+                      } );
+      
+      
       data.itens.forEach(item => {
         const planitem: ISelectItem = {
           value: item.id,
@@ -109,6 +121,17 @@ export class ProposalsComponent implements OnInit {
         }
         this.planItemDropdow.push(planitem)
       });
+      
+      
+      data.localities.sort( function( a, b ) {
+                        if ( a.name < b.name ){
+                          return -1;
+                        }
+                        if ( a.name > b.name ){
+                          return 1;
+                        }
+                        return 0;
+                      } );
 
       data.localities.forEach(locality => {
         const local: ISelectItem = {
